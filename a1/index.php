982c0ba0fd3a201693b56a744becc454cd944564
gangsjou <65882799+gangsjou@users.noreply.github.com>
2021-06-20 17:49:38 +1000

    Last and final commit for A1a - more final than the last one - correcting conflict

--- a1/index.php
+++ a1/index.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
 <html lang='en'>
+
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Assignment 2</title>
+    <meta name="author" content="Ida Gange">
+    <meta name="description" content="Ultimate Protection Sports Fencing Equipment Website: provides 
+      protective clothing and fencing equipment for beginners">
+
+    <title>Assignment 1b</title>
 
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
     <script src='../wireframe.js'></script>
+    <link rel="icon" href="../../media/upsfeLogo16x16.png" type="image/x-icon">   
   </head>
 
   <body>
 
     <header>
-      <div>Put company logo and name here</div>
+      div class="flex-container"
+        <div><img src='../../media/upsfeLogo201x201.png' alt='Logo depicts two stylised fencers
+             and company initials'></div>   
+        <div><h1>Ultimage Protection Sports Equipment</h1></div> 
     </header>
 
     <nav>
@@ -31,7 +40,7 @@
     <footer>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      </script> Name: Sjoukje Ida Gange, student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
